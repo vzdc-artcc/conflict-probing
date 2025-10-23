3b9e6ddd0625ed1a59e45bac3f5087729e7b7871
Aneesh Reddy <75503096+beabravedude@users.noreply.github.com>
2025-10-22 23:57:37 -0400

    update geographic bounds and altitude limit in config

--- config.py
+++ config.py
@@ -4,31 +4,28 @@ REPEAT_TIME=15
 
 VATSIM_DATA_URL = "https://data.vatsim.net/v3/vatsim-data.json"
 
-# Geographic bounds for monitoring
-# (bottom-left latitude, bottom-left longitude)
-BOTTOM_LEFT_LIMIT = (33.5, -82.5)
-# (top-right latitude, top-right longitude)
-TOP_RIGHT_LIMIT = (41, -71.5)
+# USA Coords
+BOTTOM_LEFT_LIMIT = (32, -130)
+TOP_RIGHT_LIMIT = (48, -62.6)
 
-# Aircraft filtering
-ALTITUDE_LIMIT_FT = 18000  # Minimum altitude to track (FL180)
+# ZDC Coords
+# BOTTOM_LEFT_LIMIT = (33.5, -82.5)
+# TOP_RIGHT_LIMIT = (41, -71.5)
+
+ALTITUDE_LIMIT_FT = 10000
 VS_ZERO_RANGE = (-150, 150)
 
-# Waypoint and route settings
-WAYPOINT_TOLERANCE_NM = 4  # Distance from route considered "on course"
+WAYPOINT_TOLERANCE_NM = 4
 
-# Prediction settings
-PREDICTION_MINUTES_AHEAD = 10  # How far ahead to predict
-PREDICTION_PRECISION_MINUTES = 1  # Time increment for predictions
+PREDICTION_MINUTES_AHEAD = 10
+PREDICTION_PRECISION_MINUTES = 1
 
-# Separation thresholds
-LATERAL_SEPARATION_RED_NM = 5.0  # Red alert threshold
-VERTICAL_SEPARATION_RED_FT = 1000.0  # Red alert threshold
-LATERAL_SEPARATION_YELLOW_NM = 12.0  # Yellow alert threshold
-VERTICAL_SEPARATION_YELLOW_FT = 1000.0  # Yellow alert threshold
-VERTICAL_TOLERANCE_FT = 150  # Added margin for vertical calculations
+LATERAL_SEPARATION_RED_NM = 5.0
+VERTICAL_SEPARATION_RED_FT = 1000.0
+LATERAL_SEPARATION_YELLOW_NM = 12.0
+VERTICAL_SEPARATION_YELLOW_FT = 1000.0
+VERTICAL_TOLERANCE_FT = 150
 
-# FAA NASR Data
 NAVDATA_PATH = "navdata_feather/"
 FIX_FILE = NAVDATA_PATH + "FIX_BASE.feather"
 NAV_FILE = NAVDATA_PATH + "NAV_BASE.feather"
